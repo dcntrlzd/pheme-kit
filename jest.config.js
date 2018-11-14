f88f8d8151bdc0fa03739deb6069559f9d960228
Baris Gumustas <barisgumustas@gmail.com>
2018-11-14 23:36:12 +0100

    Working on typescript and jest setup.

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "testEnvironment": "node",
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node",
+    "sol"
+  ],
+  "moduleDirectories": [
+    "node_modules",
+    "packages"
+  ]
+};
