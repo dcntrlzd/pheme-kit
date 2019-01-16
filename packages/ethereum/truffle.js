11274963bc8803e6acf06847f1e323ac4478006f
Baris Gumustas <barisgumustas@gmail.com>
2019-01-16 21:15:47 +0100

    Added truffle artifacts to the ethereum package.

--- packages/ethereum/truffle.js
+++ packages/ethereum/truffle.js
@@ -1,7 +1,10 @@
 require('ts-node').register();
 
+const path = require('path');
+
 module.exports = {
   test_file_extension_regexp: /.*\.[tj]s$/,
+  contracts_build_directory: path.join(__dirname, 'artifacts'),
   compilers: {
     solc: {
       version: '0.4.25',
